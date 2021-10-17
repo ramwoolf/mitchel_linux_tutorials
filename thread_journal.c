703dfb6ea2b30dc52b708218ca5e3ce41d05b69a
Aleksandr Volkov <ramwoolf@gmail.com>
2021-10-17 23:46:45 +0200

    Remove printfs, fix format specifier

--- thread_journal.c
+++ thread_journal.c
@@ -5,18 +5,15 @@
 static pthread_key_t thread_log_key;
 
 void write_to_thread_log(char const* msg) {
-    printf("Write log in %d\n", (int)pthread_self());
     FILE* thread_log = (FILE*)pthread_getspecific(thread_log_key);
-    fprintf(thread_log, "%S\n", msg);
+    fprintf(thread_log, "%s\n", msg);
 }
 
 void close_thread_log(void* thread_log) {
-    printf("Close file in %d\n", (int)pthread_self());
     fclose((FILE*)thread_log);
 }
 
 void* thread_function(void* args) {
-    printf("HERE %d\n", (int)pthread_self());
     char thread_lof_filename[20];
     FILE* thread_log;
 
@@ -27,15 +24,11 @@ void* thread_function(void* args) {
     write_to_thread_log("Start log.");
     write_to_thread_log("End log.");
     return NULL;
-    // return thread_log;
 }
 
 int main(int argc, char const *argv[])
 {
     int i = 0;
-    // void* log = thread_function(NULL);
-    // close_thread_log(log);
-
     pthread_t threads[5];
     pthread_key_create(&thread_log_key, close_thread_log);
     
