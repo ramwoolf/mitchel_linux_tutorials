381b6a0502d6706489d22227db4a2812008b087f
Aleksandr Volkov <ramwoolf@gmail.com>
2021-10-29 01:01:06 +0200

    Add producer thread function

--- producers_consumers.c
+++ producers_consumers.c
@@ -3,14 +3,17 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
 
-typedef struct {
+typedef struct job_t {
     __uint16_t working_time_seconds;
+    struct job_t* next;
 } job_t;
 
 typedef struct {
-    job_t* first;
-    job_t* last;
+    struct job_t* first;
+    struct job_t* last;
 } job_queue_t;
 
 void job_queue_ctor(job_queue_t *queue) {
@@ -18,26 +21,67 @@ void job_queue_ctor(job_queue_t *queue) {
     queue->last = NULL;
 }
 
-bool add_task_to_queue() {
-    //
+void job_queue_dtor(job_queue_t *queue) {
+    while (queue->first != queue->last) {
+        job_t* temp = queue->first;
+        queue->first = queue->first->next;
+        free(temp);
+    }
+}
+
+bool add_task_to_queue(job_queue_t *queue) {
+    // queue->last
 }
 
 
 job_queue_t job_queue;
 pthread_mutex_t job_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t job_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t done_job_count_mutex = PTHREAD_MUTEX_INITIALIZER;
+int job_count = 0;
+int done_job_count = 0;
 
-void producer_function() {
-    //
+void * producer_function(void *number) {
+    int producer_number = *((int*)number);
+    while (1) {
+        pthread_mutex_lock(&job_count_mutex);
+        if (job_count > 0) {
+            --job_count;
+            pthread_mutex_unlock(&job_count_mutex);
+            sleep(1 + (rand() % 10));
+            job_t *new_job = malloc(sizeof(job_t));
+
+            if (new_job == NULL) {
+                printf("Producer %d cannot create a new task and finished\n", producer_number);
+                return NULL;
+            }
+
+            new_job->next = NULL;
+            new_job->working_time_seconds = 1 + (rand() % 10);
+
+            printf("Produce %d add new task to queue\n", producer_number);
+
+            pthread_mutex_lock(&job_queue_mutex);
+            job_queue.last->next = new_job;
+            pthread_mutex_unlock(&job_queue_mutex);
+        }
+        else {
+            pthread_mutex_unlock(&job_count_mutex);
+            return NULL;
+        }
+    }
+    return NULL;
 }
 
-void consumer_function() {
-    //
+void * consumer_function(void *number) {
+    int consumer_number = *((int*)number);
+    return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 3) {
-        printf("Provide two integer arguments\n");
+    if (argc < 4) {
+        printf("Provide three integer arguments\n");
         return -1;
     }
 
@@ -45,6 +89,9 @@ int main(int argc, char const *argv[])
 
     int producers_count = atoi(argv[1]);
     int consumers_count = atoi(argv[2]);
+    job_count = atoi(argv[3]);
+
+    srand(time(NULL));
     
     pthread_t *producers = NULL;
     pthread_t *consumers = NULL;
@@ -52,12 +99,14 @@ int main(int argc, char const *argv[])
     producers = malloc(producers_count * sizeof(pthread_t));
     if (producers == NULL) {
         printf("Cannot allocate memory for threads\n");
+        job_queue_dtor(&job_queue);
         return -1;
     }
     consumers = malloc(consumers_count * sizeof(pthread_t));
     if (consumers == NULL) {
         free(producers);
         printf("Cannot allocate memory for threads\n");
+        job_queue_dtor(&job_queue);
         return -1;
     }
 
@@ -80,6 +129,9 @@ int main(int argc, char const *argv[])
 
     free(producers);
     free(consumers);
+    job_queue_dtor(&job_queue);
+
+    printf("Job count is %d\n Done %d tasks\n", job_count, done_job_count);
     return 0;
 }
 
