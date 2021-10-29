19810598062774f7f7228b0a982525f30c500f4e
Aleksandr Volkov <ramwoolf@gmail.com>
2021-10-29 13:51:24 +0200

    Fix segfault, add traces macro, disable consumers

--- producers_consumers.c
+++ producers_consumers.c
@@ -6,6 +6,17 @@
 #include <time.h>
 #include <unistd.h>
 
+// #define TRACE
+#ifdef TRACE
+#define TRACE_FUNC printf("Enter to function %s()\n", __func__)
+#define TRACE_LINE printf("Pass line %d in func %s()\n", __LINE__, __func__)
+#define TRACE_THREAD_LINE(thread_number) printf("Thread %d, Pass line %d in func %s()\n", thread_number, __LINE__, __func__)
+#else
+#define TRACE_FUNC
+#define TRACE_LINE
+#define TRACE_THREAD_LINE(thread_number)
+#endif
+
 typedef struct job_t {
     __uint16_t working_time_seconds;
     struct job_t* next;
@@ -14,11 +25,19 @@ typedef struct job_t {
 typedef struct {
     struct job_t* first;
     struct job_t* last;
+    int size;
 } job_queue_t;
 
-void job_queue_ctor(job_queue_t *queue) {
-    queue->first = NULL;
-    queue->last = NULL;
+bool job_queue_ctor(job_queue_t *queue) {
+    queue->first = malloc(sizeof(job_t));
+    if (queue->first == NULL) {
+        return false;
+    }
+    queue->last = queue->first;
+    queue->first->working_time_seconds = -1;
+    queue->first->next = NULL;
+    queue->size = 0;
+    return true;
 }
 
 void job_queue_dtor(job_queue_t *queue) {
@@ -29,11 +48,6 @@ void job_queue_dtor(job_queue_t *queue) {
     }
 }
 
-bool add_task_to_queue(job_queue_t *queue) {
-    // queue->last
-}
-
-
 job_queue_t job_queue;
 pthread_mutex_t job_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t job_count_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -42,13 +56,19 @@ int job_count = 0;
 int done_job_count = 0;
 
 void * producer_function(void *number) {
+    TRACE_FUNC;
     int producer_number = *((int*)number);
+    TRACE_THREAD_LINE(producer_number);
+
     while (1) {
+        TRACE_THREAD_LINE(producer_number);
         pthread_mutex_lock(&job_count_mutex);
         if (job_count > 0) {
             --job_count;
             pthread_mutex_unlock(&job_count_mutex);
             sleep(1 + (rand() % 10));
+
+            pthread_mutex_lock(&job_queue_mutex);
             job_t *new_job = malloc(sizeof(job_t));
 
             if (new_job == NULL) {
@@ -61,8 +81,9 @@ void * producer_function(void *number) {
 
             printf("Produce %d add new task to queue\n", producer_number);
 
-            pthread_mutex_lock(&job_queue_mutex);
+            
             job_queue.last->next = new_job;
+            ++job_queue.size;
             pthread_mutex_unlock(&job_queue_mutex);
         }
         else {
@@ -74,22 +95,29 @@ void * producer_function(void *number) {
 }
 
 void * consumer_function(void *number) {
+    TRACE_FUNC;
     int consumer_number = *((int*)number);
     return NULL;
 }
 
 int main(int argc, char const *argv[])
 {
+    TRACE_FUNC;
     if (argc < 4) {
         printf("Provide three integer arguments\n");
         return -1;
     }
 
-    job_queue_ctor(&job_queue);
+    if (job_queue_ctor(&job_queue) == false) {
+        printf("Cannot create a job queue\n");
+        return -1;
+    }
 
     int producers_count = atoi(argv[1]);
     int consumers_count = atoi(argv[2]);
     job_count = atoi(argv[3]);
+
+    printf("Job count is %d\n", job_count);
 
     srand(time(NULL));
     
@@ -110,28 +138,40 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+    TRACE_LINE;
+
     int i = 0;
     for (i = 0; i < producers_count; ++i) {
-        pthread_create(&(producers[i]), NULL, producer_function, NULL);
+        pthread_create(&(producers[i]), NULL, producer_function, &i);
     }
 
-    for (i = 0; i < consumers_count; ++i) {
-        pthread_create(&(consumers[i]), NULL, producer_function, NULL);
-    }
+    TRACE_LINE;
+
+    // for (i = 0; i < consumers_count; ++i) {
+    //     pthread_create(&(consumers[i]), NULL, producer_function, &i);
+    // }
+
+    TRACE_LINE;
 
     for (i = 0; i < producers_count; ++i) {
+        TRACE_LINE;
         pthread_join(producers[i], NULL);
     }
 
-    for (i = 0; i < consumers_count; ++i) {
-        pthread_join(producers[i], NULL);
-    }
+    // TRACE_LINE;
+
+    // for (i = 0; i < consumers_count; ++i) {
+    //     pthread_join(consumers[i], NULL);
+    // }
+
+    TRACE_LINE;
+
+    printf("Job count is %d\nDone %d tasks\n", job_count, done_job_count);
+    printf("Queue size is %d\n", job_queue.size);
 
     free(producers);
     free(consumers);
     job_queue_dtor(&job_queue);
-
-    printf("Job count is %d\n Done %d tasks\n", job_count, done_job_count);
     return 0;
 }
 
